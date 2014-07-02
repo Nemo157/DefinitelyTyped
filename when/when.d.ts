c655dc80564d1e62f9d56e9f504469e433fbb9e3
Wim Looman <ghostunderscore@gmail.com>
2014-07-02 19:39:06 +1200

    Export when as an ambient external declaration

--- when/when.d.ts
+++ when/when.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Derek Cicerone <https://github.com/derekcicerone>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-declare module When {
+declare module "when" {
 
     /**
      * Return a promise that will resolve only once all the supplied promisesOrValues
@@ -78,5 +78,3 @@ declare module When {
         reason?: any;
     }
 }
-
-export = When;
